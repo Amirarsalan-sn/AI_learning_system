5655bb59c07c029e6a06f5e3c5e2d19eef785d02
Amir Arsalan Sanati <amirarsalan.sanati81@gmail.com>
2023-11-23 22:54:58 +0330

    signup procedure tested and was succesfull.

--- ai_education/userApp/views.py
+++ ai_education/userApp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from .serializers import SignupSerializer
 # Create your views here.
 """
 You may want to change the userApp view to a class based view.
@@ -12,7 +14,14 @@ You may want to change the userApp view to a class based view.
 # the signin procedure.
 @api_view(['POST'])
 def signin(request):
-    return HttpResponse('User signin.')
+    serializer = SignupSerializer(data=request.data)
+    if serializer.is_valid():
+        """If the validation success, it will created a new user."""
+        serializer.save()
+        res = {'status': status.HTTP_201_CREATED}
+        return Response(res, status=status.HTTP_201_CREATED)
+    res = {'status': status.HTTP_400_BAD_REQUEST, 'data': serializer.errors}
+    return Response(res, status=status.HTTP_400_BAD_REQUEST)
 
 
 # the login procedure.
