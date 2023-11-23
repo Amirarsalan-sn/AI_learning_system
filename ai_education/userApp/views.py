760e7d87d7bc969d90097a794d8c7453fcf3ca9f
Amir Arsalan Sanati <amirarsalan.sanati81@gmail.com>
2023-11-23 22:41:21 +0330

    rest_frame work installed, views.py changed according to rest_frame work and singup selrializer implemented.

--- ai_education/userApp/views.py
+++ ai_education/userApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 """
 You may want to change the userApp view to a class based view.
@@ -8,17 +10,21 @@ You may want to change the userApp view to a class based view.
 
 
 # the signin procedure.
+@api_view(['POST'])
 def signin(request):
     return HttpResponse('User signin.')
 
 
 # the login procedure.
+@api_view(['POST'])
 def login(request):
     return HttpResponse('User login.')
 
 
 # the logout procedure.
+@api_view(['POST'])
 def logout(request):
     return HttpResponse('User logout.')
+
 
 
