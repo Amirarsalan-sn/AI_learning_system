ffe9587f426e931708ec02ac765aba762f2fbd3a
Mohammad Fallah <fallahmohammadm@gmail.com>
2023-11-24 04:48:20 +0330

    add frontend initial files

---
+++ front_end/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import rtlPlugin from 'stylis-plugin-rtl';
+import {prefixer} from 'stylis';
+import {CacheProvider} from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+
+});
+
+const theme = createTheme({
+    direction: 'rtl',
+    typography: {
+        fontFamily: [
+            'vazir',
+            'serif',
+        ].join(','),
+    },
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <CacheProvider value={cacheRtl}>
+                <ThemeProvider theme={theme}>
+                    <App/>
+                </ThemeProvider>
+            </CacheProvider>
+        </BrowserRouter>
+
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
