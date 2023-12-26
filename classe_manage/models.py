3ef9b06922b75c112e71a54c62521dba01fba967
Mohammad Fallah <fallahmohammadm@gmail.com>
2023-12-27 02:55:49 +0330

    edit models

--- classe_manage/models.py
+++ classe_manage/models.py
@@ -2,23 +2,54 @@ from django.db import models
 from userApp.models import CustomUser
 
 
-# Create your models here.
 class ClassRoom(models.Model):
-    class_title = models.CharField(max_length=50)
-    class_attendants = models.ManyToManyField(CustomUser)
+    ClassName = models.CharField(max_length=255)
+    ProfessorID = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    students = models.ManyToManyField(CustomUser, related_name='classes')
+    assistants = models.ManyToManyField(CustomUser, related_name='classes')
+
+    def __str__(self):
+        return self.ClassName
 
 
-class ChatRoom(models.Model):
-    chat_room_title = models.CharField(max_length=70)
-    chat_class = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+class Reply(models.Model):
+    body = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    discussion = models.ForeignKey(related_name='discussion_replies')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Reply by {self.author.username}"
 
 
-class Chat(models.Model):
-    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-    # im not sure about this on change it if you have a better idea:
-    chat_author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    chat_body = models.CharField(max_length=255)
-    created_time = models.DateTimeField(auto_now_add=True)
+class Discussion(models.Model):
+    title = models.CharField(max_length=255)
+    question = models.TextField()
+    creator = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    classroom = models.OneToOneField(ClassRoom, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def author_name(self):
-        return self.chat_author.username
+    def __str__(self):
+        return self.title
+
+
+class Exercise(models.Model):
+    exercise_name = models.CharField(max_length=255)
+    description = models.CharField(max_length=512)
+    created_at = models.DateTimeField(auto_now_add=True)
+    exercise_file = models.FileField(null=True, blank=True)
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ExerciseName
+
+
+class Submission(models.Model):
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    text = models.TextField(null=True, blank=True)
+    submission_file = models.FileField(null=True, blank=True)
+    submission_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Submission {self.SubmissionID} by {self.StudentID.username} for {self.ExerciseID.ExerciseName}"
--- classe_manage/serializers.py
+++ classe_manage/serializers.py
@@ -1,37 +1,37 @@
-from rest_framework import serializers
-from .models import ClassRoom, ChatRoom, Chat
-from ..userApp.serializers import SimpleUserSerializer
-
-
-# because this serializer is used only when we are creating a classroom, and when we are creating a classroom there
-# is only one attendant, I think there would be no problem if we just use SimpleUserSerializer().
-# but in case we wanted to send all the attendants of a class along with its title an id in one response, we can use it.
-class ClassRoomCreateSerializer(serializers.ModelSerializer):
-    class_attendants = SimpleUserSerializer(many=True)
-
-    class Meta:
-        model = ClassRoom
-        fields = ('id', 'class_title', 'class_attendants')
-
-
-class ClassRoomGetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ClassRoom
-        fields = ('id', 'class_title')
-
-
-class ChatRoomSerializer(serializers.ModelSerializer):
-    chat_class = ClassRoomGetSerializer()
-
-    class Meta:
-        model = ChatRoom
-        fields = ('id', 'chat_class')
-
-
-class ChatSerializer(serializers.ModelSerializer):
-    chat_room = ChatRoomSerializer()
-    chat_author = SimpleUserSerializer()
-
-    class Meta:
-        model = Chat
-        fields = ('id', 'chat_room', 'chat_author', 'chat_body', 'created_time')
+# from rest_framework import serializers
+# from .models import ClassRoom, ChatRoom, Chat
+# from ..userApp.serializers import SimpleUserSerializer
+#
+#
+# # because this serializer is used only when we are creating a classroom, and when we are creating a classroom there
+# # is only one attendant, I think there would be no problem if we just use SimpleUserSerializer().
+# # but in case we wanted to send all the attendants of a class along with its title an id in one response, we can use it.
+# class ClassRoomCreateSerializer(serializers.ModelSerializer):
+#     class_attendants = SimpleUserSerializer(many=True)
+#
+#     class Meta:
+#         model = ClassRoom
+#         fields = ('id', 'class_title', 'class_attendants')
+#
+#
+# class ClassRoomGetSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ClassRoom
+#         fields = ('id', 'class_title')
+#
+#
+# class ChatRoomSerializer(serializers.ModelSerializer):
+#     chat_class = ClassRoomGetSerializer()
+#
+#     class Meta:
+#         model = ChatRoom
+#         fields = ('id', 'chat_class')
+#
+#
+# class ChatSerializer(serializers.ModelSerializer):
+#     chat_room = ChatRoomSerializer()
+#     chat_author = SimpleUserSerializer()
+#
+#     class Meta:
+#         model = Chat
+#         fields = ('id', 'chat_room', 'chat_author', 'chat_body', 'created_time')
