71e1717999361d204b19705209b142fb476a4d48
AlirezaAmini8 <alirezaamini.138115@gmail.com>
2023-12-27 05:32:15 +0330

    Content component fixed.

--- front_end/src/Components/Landing/SubComponents/Contact.jsx
+++ front_end/src/Components/Landing/SubComponents/Contact.jsx
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
-import { Container, Grid, Typography, TextField, TextareaAutosize, Button } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import Box from "@mui/material/Box";
-
-const initialState = {
-    name: '',
-    email: '',
-    message: '',
-};
-
 const Contact = (props) => {
     return (
         <Box id='contact' >
