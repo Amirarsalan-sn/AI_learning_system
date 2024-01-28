9e14dee69a15758fdc6ffaf04fa841cc738bdf61
Amir Arsalan Sanati <amirarsalan.sanati81@gmail.com>
2024-01-28 18:33:31 +0330

    code reviewed.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """
     This code is written by Omid Reza Borzoyi.
+
+    This code is reviewed and edited by Amir Arsalan Sanati.
 """
 
-ROOT = 0  # node 0 is always the root
 sample_graph = {
     # parent: list of children from left to right
     0: [1, 2],
@@ -14,78 +15,82 @@ sample_graph = {
 }
 
 
-def show_graph(graph: dict[int, list[int]]):
-    for key, value in graph.items():
-        print(key, value)
+class SearchAlgos:
+    _ROOT = 0
 
+    @staticmethod
+    def _show_graph(graph: dict[int, list[int]]):
+        for key, value in graph.items():
+            print(key, value)
 
-def get_all_nodes(graph: dict[int, list[int]]):
-    maximum = -float('inf')
-    for children in graph.values():
-        maximum = max(maximum, max(children))
-    return list(range(maximum + 1))
+    @staticmethod
+    def _get_all_nodes(graph: dict[int, list[int]]):
+        maximum = -float('inf')
+        for children in graph.values():
+            maximum = max(maximum, max(children))
+        return list(range(maximum + 1))
 
+    @staticmethod
+    def bfs(graph: dict, priority='left_to_right') -> list[int]:
+        nodes = SearchAlgos._get_all_nodes(graph)
+        visited = [False] * len(nodes)
+        sequence = []
 
-def bfs(graph: dict, priority='left_to_right') -> list[int]:
-    nodes = get_all_nodes(graph)
-    visited = [False] * len(nodes)
-    sequence = []
+        if priority == 'left_to_right':
+            parents_to_check = [SearchAlgos._ROOT]
 
-    if priority == 'left_to_right':
-        parents_to_check = [ROOT]
+            while parents_to_check:
+                parent = parents_to_check.pop(0)
 
-        while parents_to_check:
-            parent = parents_to_check.pop(0)
+                if not visited[parent]:
+                    sequence.append(parent)
+                    visited[parent] = True
 
-            if not visited[parent]:
-                sequence.append(parent)
-                visited[parent] = True
+                try:
+                    children = graph[parent]
 
+                    for child in children:
+                        sequence.append(child)
+                        parents_to_check.append(child)
+                        visited[child] = True
+
+                except KeyError:
+                    pass
+
+        return sequence
+
+    @staticmethod
+    def _dfs(graph: dict[int, list[int]], root: int, visited: list[bool], priority: str, sequence: list):
+        if visited[root]:
+            return
+
+        sequence.append(root)
+        visited[root] = True
+
+        if priority == 'left_to_right':
             try:
-                children = graph[parent]
+                children = graph[root]
 
                 for child in children:
-                    sequence.append(child)
-                    parents_to_check.append(child)
-                    visited[child] = True
+                    SearchAlgos._dfs(graph, child, visited, priority, sequence)
 
             except KeyError:
                 pass
 
-    return sequence
+    @staticmethod
+    def dfs(graph: dict[int, list[int]], priority='left_to_right') -> list[int]:
+        nodes = SearchAlgos._get_all_nodes(graph)
+        visited = [False] * len(nodes)
+        sequence = []
 
+        SearchAlgos._dfs(graph, SearchAlgos._ROOT, visited, priority, sequence)
 
-def _dfs(graph: dict[int, list[int]], root: int, visited: list[bool], priority: str, sequence: list):
-    if visited[root]:
-        return
-
-    sequence.append(root)
-    visited[root] = True
-
-    if priority == 'left_to_right':
-        try:
-            children = graph[root]
-
-            for child in children:
-                _dfs(graph, child, visited, priority, sequence)
-
-        except KeyError:
-            pass
-
-
-def dfs(graph: dict[int, list[int]], priority='left_to_right') -> list[int]:
-    nodes = get_all_nodes(graph)
-    visited = [False] * len(nodes)
-    sequence = []
-
-    _dfs(graph, ROOT, visited, priority, sequence)
-
-    return sequence
+        return sequence
 
 
 if __name__ == "__main__":
-    dfs_seq = dfs(sample_graph)
-    bfs_seq = bfs(sample_graph)
+    dfs_seq = SearchAlgos.dfs(sample_graph)
+    bfs_seq = SearchAlgos.bfs(sample_graph)
 
     print(dfs_seq)
     print(bfs_seq)
