709d83eab039e395384f9d36c1072a0768a0f7b4
asus <amir.eftekhari1280@gmail.com>
2024-01-28 22:14:09 +0330

    Complete ClassAPIView test.

--- classe_manage/tests.py
+++ classe_manage/tests.py
@@ -17,3 +17,24 @@ class ClassAPIViewTests(TestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.get('/api/classes/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_class(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        data = {'name': 'New Class', 'ProfessorID': self.user.id}
+        response = self.client.post('/api/classes/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ClassRoom.objects.count(), 2)
+
+    def test_update_class(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        data = {'name': 'Updated Class', 'ProfessorID': self.user.id}
+        response = self.client.put(f'/api/classes/{self.classroom.id}/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.classroom.refresh_from_db()
+        self.assertEqual(self.classroom.name, 'Updated Class')
+
+    def test_delete_class(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.delete(f'/api/classes/{self.classroom.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(ClassRoom.objects.count(), 0)
