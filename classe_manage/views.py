c44f12f8cdd83fe850b7b8254c43af6caadf3529
asus <amir.eftekhari1280@gmail.com>
2024-01-28 21:32:10 +0330

    Clean the code.

--- classe_manage/views.py
+++ classe_manage/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
