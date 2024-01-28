de7ee77364fa6a2abfc6fd722fe43a96adb4571a
asus <amir.eftekhari1280@gmail.com>
2024-01-28 22:18:01 +0330

    Complete DiscussionAPIView tests.

--- classe_manage/tests.py
+++ classe_manage/tests.py
@@ -38,3 +38,27 @@ class ClassAPIViewTests(TestCase):
         response = self.client.delete(f'/api/classes/{self.classroom.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(ClassRoom.objects.count(), 0)
+
+
+class DiscussionAPIViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.token = Token.objects.create(user=self.user)
+        self.classroom = ClassRoom.objects.create(name='Test Class', ProfessorID=self.user.id)
+        self.discussion = Discussion.objects.create(title='Test Discussion', creator=self.user, classroom=self.classroom)
+
+    def test_get_discussion_list(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.get(f'/api/discussions/{self.classroom.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_discussion_detail(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.get(f'/api/discussions/{self.classroom.id}/{self.discussion.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_discussion(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        data = {'title': 'New Discussion', 'creator': self.user.id, 'classroom': self.classroom.id}
+        response = self.client.post(f'/api/discussions/{self.classroom.id}/', data, format='json')
