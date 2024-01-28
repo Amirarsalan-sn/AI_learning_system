80ecddd2bd6d3c2cc25f87fa542db0d147ea9d4b
asus <amir.eftekhari1280@gmail.com>
2024-01-28 21:41:42 +0330

    Add setUp test.

--- classe_manage/tests.py
+++ classe_manage/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from .models import ClassRoom, Discussion, Reply, Exercise, Submission, Grade
+from rest_framework import status
 
-# Create your tests here.
+
+class ClassAPIViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.token = Token.objects.create(user=self.user)
+        self.classroom = ClassRoom.objects.create(name='Test Class', ProfessorID=self.user.id)
